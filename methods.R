969f3758d0352b98e081a1f89e2e5cb2bb68b081
stephens999 <stephens999@gmail.com>
2015-01-12 20:14:41 -0600

    added methods.R and score.R

---
+++ methods.R
@@ -0,0 +1,6 @@
+sourceDir("methods")
+methods=list()
+#now for each method define a list with its name, function and arguments (if no additional arguments use NULL)
+# like this: 
+#methods[[1]] = list(name="methodname",fn = function,args=NULL)
+ 
---
+++ score.R
@@ -0,0 +1,6 @@
+#This file should define your score function
+
+score = function(data, output){
+#insert calculations here; return a named list of results
+  return(list())
+}
